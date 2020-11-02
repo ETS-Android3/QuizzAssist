daf7bf46c0d6f5911d816b12a3d840d63b10d043
Brandon Yip <brandonyip790@gmail.com>
2020-11-01 18:54:03 -0800

    trying again. added some methods.

---
+++ Student.cc
@@ -0,0 +1,12 @@
+class Teacher {
+    string studentGrade;
+    void moveStudent(int x, int y) {
+        //move the student to {x, y}
+    int x = 5;
+    }
+
+    int evaluateGradePercentage(char letterGrade) {
+        studentGrade == letterGrade;
+    }
+};
+
---
+++ testclass.cc
@@ -0,0 +1,4 @@
+
+class tempClass {
+    int t;
+};

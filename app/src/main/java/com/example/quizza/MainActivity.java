5153c8d375b8193e1c367cf9628e773871de6407
Vatsal <vparmar@sfu.ca>
2020-11-08 01:03:46 -0500

    Added Animations for Login page and solved some UI issues

--- app/src/main/java/com/example/quizza/MainActivity.java
+++ app/src/main/java/com/example/quizza/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.quizza;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 

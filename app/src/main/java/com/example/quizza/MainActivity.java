270ec9c2d2cec9f88946bc5acdc24aa293a1049a
Vatsal <vparmar@sfu.ca>
2020-11-09 03:07:41 -0500

    Working properly with Adjusted UI

--- app/src/main/java/com/example/quizza/MainActivity.java
+++ app/src/main/java/com/example/quizza/MainActivity.java
@@ -27,6 +27,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 
@@ -111,7 +112,6 @@ public class MainActivity extends AppCompatActivity {
         deleteCourses.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
             }
         });
 

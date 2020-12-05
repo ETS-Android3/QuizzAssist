86f2459ee5aa080377b972691664c59652b15f45
Ayeon <yeonandrew87@gmail.com>
2020-12-05 15:49:17 -0800

    updated

--- app/src/main/java/com/example/quizza/WhiteBoard.java
+++ app/src/main/java/com/example/quizza/WhiteBoard.java
@@ -103,7 +103,6 @@ public class WhiteBoard extends Activity {
         final MyView myView = new MyView(this);
         container.addView(myView);
 
-        ArrayList<String> questionList = getIntent().getStringArrayListExtra("questions");
         String questionTitle = getIntent().getStringExtra("questionTitle");
 
         FirebaseDatabase.getInstance().getReference("Questions").addListenerForSingleValueEvent(new ValueEventListener() {

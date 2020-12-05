6a355ff25e9d9a800ec61a7bf28ef943037c0289
Ayeon <yeonandrew87@gmail.com>
2020-12-04 22:43:53 -0800

    updated UI

--- app/src/main/java/com/example/quizza/EventDetailsFragment.java
+++ app/src/main/java/com/example/quizza/EventDetailsFragment.java
@@ -67,7 +67,6 @@ public class EventDetailsFragment extends Fragment {
                     }
                 }
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 

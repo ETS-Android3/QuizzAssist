fbfd28db307465b92bb60f9c53d0664b9da47bbb
Brandon Yip <brandonyip790@gmail.com>
2020-12-06 23:05:26 -0800

    implementing user avatar taking photo

--- app/src/main/java/com/example/quizza/SettingsFragment.java
+++ app/src/main/java/com/example/quizza/SettingsFragment.java
@@ -344,6 +344,7 @@ public class SettingsFragment extends BottomSheetDialogFragment {
                 Log.d("finished", "end of dispatch");
             }
         }
+        //goes here
     }
 
     public File createPhotoFile() throws IOException {
